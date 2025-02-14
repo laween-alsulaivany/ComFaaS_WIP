ce7cf7d0843c52b491042a0f0094d8fae0783ce4
Laween Al Sulaivany <78901349+laween-alsulaivany@users.noreply.github.com>
2025-02-14 01:16:51 -0600

    fixed Bigsummation directoy

--- tests/SamplePrograms/BigSummation.py
+++ tests/SamplePrograms/BigSummation.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import os
 
 
 def big_summation(n):
@@ -14,6 +15,9 @@ if __name__ == "__main__":
     # Summation up to 50 million for a decent CPU load
     result = big_summation(50_000_000)
     elapsed = time.time() - start_time
+
+    OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "..", "Output")
+
     # Replace console output with file output
-    with open("../Output/BigSummation.txt", "w") as f:
+    with open(os.path.join(OUTPUT_PATH, "BigSummation.txt"), "w") as f:
         f.write(f"Result is {result}, took {elapsed:.2f} seconds.")
